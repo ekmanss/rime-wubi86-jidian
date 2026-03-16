bc84b18f301ad5fab31b0d3ea1665fca6d771e6e
heshilin <hfers@msn.com>
2026-03-16 16:25:25 +0800

    all done

--- lua/wubi86_jidian_date_translator.lua
+++ lua/wubi86_jidian_date_translator.lua
@@ -23,13 +23,13 @@ function wubi86_jidian_date_translator(input, seg)
     -- %y	two-digit year (98) [00-99]
     -- %%	the character `%´
 
-    -- 输入完整日期
-    if (input == "datetime") then
+    -- 输入完整日期：触发词 rqsj
+    if (input == "rqsj") then
         yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d %H:%M:%S"), ""))
     end
 
-    -- 输入日期
-    if (input == "date") then
+    -- 输入日期：触发词 rq
+    if (input == "rq") then
         --- Candidate(type, start, end, text, comment)
         yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%Y.%m.%d"), ""))
@@ -39,8 +39,8 @@ function wubi86_jidian_date_translator(input, seg)
         yield(Candidate("date", seg.start, seg._end, os.date("%m-%d-%Y"), ""))
     end
 
-    -- 输入时间
-    if (input == "time") then
+    -- 输入时间：触发词 sj
+    if (input == "sj") then
         --- Candidate(type, start, end, text, comment)
         yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), ""))
         yield(Candidate("time", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), ""))
@@ -48,10 +48,10 @@ function wubi86_jidian_date_translator(input, seg)
         yield(Candidate("time", seg.start, seg._end, os.date("%H%M%S"), ""))
     end
 
-    -- 输入星期
+    -- 输入星期：触发词 xq
     -- -- @JiandanDream
     -- -- https://github.com/KyleBing/rime-wubi86-jidian/issues/54
-    if (input == "week") then
+    if (input == "xq") then
         local weekTab = {'日', '一', '二', '三', '四', '五', '六'}
         yield(Candidate("week", seg.start, seg._end, "周"..weekTab[tonumber(os.date("%w")+1)], ""))
         yield(Candidate("week", seg.start, seg._end, "星期"..weekTab[tonumber(os.date("%w")+1)], ""))
@@ -60,8 +60,8 @@ function wubi86_jidian_date_translator(input, seg)
         yield(Candidate("week", seg.start, seg._end, os.date("%W"), "周数"))
     end
 
-    -- 输入月份英文
-    if (input == "month") then
+    -- 输入月份英文：触发词 yf
+    if (input == "yf") then
         yield(Candidate("month", seg.start, seg._end, os.date("%B"), ""))
         yield(Candidate("month", seg.start, seg._end, os.date("%b"), "缩写"))
     end
